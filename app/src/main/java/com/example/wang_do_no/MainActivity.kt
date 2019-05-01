1c4503d12df78ceb37a65634877739d86fc71bd2
sami <ydjm1027@naver.com>
2019-05-01 22:15:03 +0900

    Main_AddButtonEvent
    Main activity에서 버튼 event 생성

--- app/src/main/java/com/example/wang_do_no/MainActivity.kt
+++ app/src/main/java/com/example/wang_do_no/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.wang_do_no
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 버튼 클릭 이벤트
+        Subway_Button.setOnClickListener {
+            val intent_subway = Intent(this, SubwayActivity::class.java)
+            startActivity(intent_subway)
+        }
+
+        SignIn_Button.setOnClickListener {
+            val intent_sign = Intent(this,SignActivity::class.java)
+            startActivity(intent_sign)
+        }
+
+        Bus_Button.setOnClickListener {
+            val intent_bus = Intent(this, BusActivity::class.java)
+            startActivity(intent_bus)
+        }
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/wang_do_no/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wang_do_no
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SubwayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
