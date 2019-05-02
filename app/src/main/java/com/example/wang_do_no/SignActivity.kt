00b02369a1c37d89b69554dcdced8e87d9d2c0dd
sami <ydjm1027@naver.com>
2019-05-02 16:04:38 +0900

    signIn_addBtn
    로그인 액티비티 간단하게 버튼추가

--- app/src/main/java/com/example/wang_do_no/SignActivity.kt
+++ app/src/main/java/com/example/wang_do_no/SignActivity.kt
@@ -1,12 +1,19 @@
 package com.example.wang_do_no
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign.*
 
 class SignActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        SignUp_Btn.setOnClickListener {
+            val intent_signup = Intent(this, SignUpActivity::class.java)
+            startActivity(intent_signup)
+        }
     }
 }
