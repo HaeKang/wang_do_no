6a9ab4be6ab326b5619d17775c326050f289ccb9
sami <ydjm1027@naver.com>
2019-05-05 01:43:31 +0900

    login_btn_add

--- app/src/main/java/com/example/wang_do_no/SignActivity.kt
+++ app/src/main/java/com/example/wang_do_no/SignActivity.kt
@@ -3,6 +3,8 @@ package com.example.wang_do_no
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign.*
 
 class SignActivity : AppCompatActivity() {
@@ -11,9 +13,19 @@ class SignActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
 
-        SignUp_Btn.setOnClickListener {
+        Login_btn.setOnClickListener {
             val intent_signup = Intent(this, SignUpActivity::class.java)
             startActivity(intent_signup)
+        }
+
+       Login_btn.setOnClickListener {
+            val inputId = User_id.text.trim().toString()
+            val inputPw = User_pw.text.trim().toString()
+
+           if(inputId.isNullOrBlank() || inputPw.isNullOrBlank()) {
+               Toast.makeText(this, "ID와 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+           }
+
         }
     }
 }
