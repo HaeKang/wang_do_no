8d065da2d3691d64ec65d621735d8be1f4f22100
sami <ydjm1027@naver.com>
2019-05-07 14:54:00 +0900

    api설정

--- app/src/main/java/com/example/wang_do_no/SubwayActivity.kt
+++ app/src/main/java/com/example/wang_do_no/SubwayActivity.kt
@@ -2,11 +2,21 @@ package com.example.wang_do_no
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_subway.*
+import kr.go.seoul.trafficsubway.TrafficSubwayButtonTypeB
+import java.sql.Types.NULL
 
 class SubwayActivity : AppCompatActivity() {
+    private val OpenApiKey = "sample"
+    private lateinit var TypeB : TrafficSubwayButtonTypeB
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subway)
+
+        TypeB = findViewById(R.id.type_b)
+        TypeB.setOpenAPIKey(OpenApiKey)
+
     }
 }
